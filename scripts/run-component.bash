5e6d392e0384ac853676b5096ff241ebc8254478
Ciprian Dorin Craciun <ciprian@volution.ro>
2012-10-17 21:55:33 +0300

    Update scripts

--- scripts/run-component.bash
+++ scripts/run-component.bash
@@ -6,7 +6,7 @@ if ! test "${#}" -le 1 ; then
 fi
 
 _identifier="${1:-00000000d9b57d874a6b414da929957594b7569e}"
-_fqdn="${mosaic_node_fqdn:-}"
+_fqdn="${mosaic_node_fqdn:-mosaic-0.loopback.vnet}"
 
 if test -n "${mosaic_component_temporary:-}" ; then
 	_tmp="${mosaic_component_temporary:-}"
@@ -18,7 +18,7 @@ fi
 
 _erl_args+=(
 		-noinput -noshell
-		-name "mosaic-couchdb-${_identifier}@${_fqdn:-mosaic-0.loopback.vnet}"
+		-name "mosaic-couchdb-${_identifier}@${_fqdn}"
 		-setcookie "${_erl_cookie}"
 		-boot start_sasl
 		-config "${_erl_libs}/mosaic_couchdb/priv/mosaic_couchdb.config"
@@ -37,26 +37,28 @@ if test "${_identifier}" != 00000000d9b57d874a6b414da929957594b7569e ; then
 			mosaic_component_harness_input_descriptor=3
 			mosaic_component_harness_output_descriptor=4
 	)
-	exec  3<&0- 4>&1- </dev/null >&2
+	exec 3<&0- 4>&1- </dev/null >&2
 else
 	_erl_args+=(
 			-run mosaic_couchdb_callbacks standalone
 	)
 fi
 
-mkdir -p "${_tmp}"
-cd "${_tmp}"
+mkdir -p -- "${_tmp}"
+cd -- "${_tmp}"
 
 if test ! -e "${_tmp}/lib" ; then
-	ln -s -T "${_erl_libs}" "${_tmp}/lib"
+	ln -s -T -- "${_erl_libs}" "${_tmp}/lib"
 fi
 
 if test ! -e "${_tmp}/data" ; then
-	mkdir "${_tmp}/data"
-	mkdir "${_tmp}/data/db"
-	mkdir "${_tmp}/data/run"
-	mkdir "${_tmp}/data/log"
-	cp -T "${_tmp}/lib/couch/priv/local.ini" "${_tmp}/data/run/local.ini"
+	mkdir -- "${_tmp}/data"
+	mkdir -- "${_tmp}/data/db"
+	mkdir -- "${_tmp}/data/run"
+	mkdir -- "${_tmp}/data/log"
+	cp -T -- "${_tmp}/lib/couch/priv/local.ini" "${_tmp}/data/run/local.ini"
 fi
 
 exec env "${_erl_env[@]}" "${_erl_bin}" "${_erl_args[@]}"
+
+exit 1
